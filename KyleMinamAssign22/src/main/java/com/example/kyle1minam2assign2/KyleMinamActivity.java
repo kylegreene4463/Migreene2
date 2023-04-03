86d9f34a92aa01e06421a0fb5c81c437679da527
Kyle Greene <kyle.greene741@gmail.com>
2023-04-03 15:57:41 -0400

    first commit

---
+++ KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamActivity.java
@@ -0,0 +1,14 @@
+package com.example.kyle1minam2assign2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KyleMinamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
