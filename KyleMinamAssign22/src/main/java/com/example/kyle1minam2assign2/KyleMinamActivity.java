a862a1737942ed260b5c064a27795df2119328d3
Kyle Greene <kyle.greene741@gmail.com>
2023-04-09 17:52:53 -0400

    Fixed action bar so it is correct

--- KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamActivity.java
+++ KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamActivity.java
@@ -3,13 +3,20 @@ package com.example.kyle1minam2assign2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class KyleMinamActivity extends AppCompatActivity {
 
@@ -17,29 +24,41 @@ public class KyleMinamActivity extends AppCompatActivity {
     ActionBarDrawerToggle actionBarDrawerToggle;
     N01534463N01451260Weather weather = new N01534463N01451260Weather();
     Migreene22Download download = new Migreene22Download();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         drawerLayout = findViewById(R.id.my_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
+        int id = item.getItemId();
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-
-
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        switch (id) {
+            case R.id.weather:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, weather).commit();
+                drawerLayout.closeDrawers();
+                return true;
+            case R.id.download:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, download).commit();
+                drawerLayout.closeDrawers();
+                return true;
+            default:
+                return false;
+        }
     }
 }
-
