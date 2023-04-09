8719d88b425c684ab520fad732235f7e0503cabf
alcantaraMinam1260 <alcantm002@gmail.com>
2023-04-09 18:37:22 -0400

    Mi Nam 2ND Commit -  Fixed nav bar issues

--- KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamActivity.java
+++ KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamActivity.java
@@ -3,6 +3,8 @@ package com.example.kyle1minam2assign2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
@@ -10,36 +12,79 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
-public class KyleMinamActivity extends AppCompatActivity {
+public class KyleMinamActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
-    DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
     ActionBarDrawerToggle actionBarDrawerToggle;
     N01534463N01451260Weather weather = new N01534463N01451260Weather();
     Migreene22Download download = new Migreene22Download();
+    KyleMinamHome kyleMinamHome = new KyleMinamHome();
+
+    DBScreen dbScreen = new DBScreen();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        // 6 - Configure all views
 
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+        this.configureToolBar();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.configureDrawerLayout();
+
+        this.configureNavigationView();
+
+    }
+    @Override
+    public void onBackPressed() {
+        // 5 - Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    private void configureToolBar(){
+        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
     }
 
+    // 2 - Configure Drawer Layout
+    private void configureDrawerLayout(){
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navOpen, R.string.navClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // 3 - Configure NavigationView
+    private void configureNavigationView(){
+        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
 
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-
-
-            return true;
+        if (id == R.id.home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_frame_layout, new KyleMinamHome()).commit();
+        } else if (id == R.id.download) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_frame_layout, new Migreene22Download()).commit();
+        } else if (id == R.id.weather) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_frame_layout, new N01534463N01451260Weather()).commit();
+        } else if (id == R.id.db) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_frame_layout,new DBScreen()).commit();
         }
-        return super.onOptionsItemSelected(item);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
 
--- KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamHome.java
+++ KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamHome.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MiNamHome#newInstance} factory method to
+ * Use the {@link KyleMinamHome#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MiNamHome extends Fragment {
+public class KyleMinamHome extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class MiNamHome extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public MiNamHome() {
+    public KyleMinamHome() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class MiNamHome extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MiNamHome.
+     * @return A new instance of fragment KyleMinamHome.
      */
     // TODO: Rename and change types and number of parameters
-    public static MiNamHome newInstance(String param1, String param2) {
-        MiNamHome fragment = new MiNamHome();
+    public static KyleMinamHome newInstance(String param1, String param2) {
+        KyleMinamHome fragment = new KyleMinamHome();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
