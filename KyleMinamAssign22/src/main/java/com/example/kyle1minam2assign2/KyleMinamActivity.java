be0fc596d84ddddd49bc01d97272dca6e8604ba0
alcantaraMinam1260 <alcantm002@gmail.com>
2023-04-09 18:58:30 -0400

    Mi Nam commit 3 : Fixed app issues

--- KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamActivity.java
+++ KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamActivity.java
@@ -39,6 +39,8 @@ public class KyleMinamActivity extends AppCompatActivity implements NavigationVi
         this.configureDrawerLayout();
 
         this.configureNavigationView();
+        getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_frame_layout, new KyleMinamHome()).commit();
+
 
     }
     @Override
--- KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamHome.java
+++ KyleMinamAssign22/src/main/java/com/example/kyle1minam2assign2/KyleMinamHome.java
@@ -59,6 +59,6 @@ public class KyleMinamHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.minam_home, container, false);
+        return inflater.inflate(R.layout.kyleminam_home, container, false);
     }
 }
